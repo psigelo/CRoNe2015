47ed56eb01ed9f7c7b6b9d5f6ec8b8f3031825c4
psigelo <pascal.sigel@gmail.com>
2015-05-25 01:14:00 -0300

    cambio menor

--- web/poster.php
+++ web/poster.php
@@ -10,27 +10,11 @@
 <!--//posters-->
 <br \>
 <div class="posterDetailConteiner">
-	<h1> Posters </h1>
-	<p><b>CRoNe 2015</b> tiene contemplado aprovechar la convocatoria de los expositores de nivel internacional para dar una realimentación a tu ingestivación, para ello se agendado un tiempo para la <b>exposición de los poster</b> en la cual además de los expositores y los asistentes al evento habrá un gran flujo de personas en la universidad por ser la semana en que la universidad se abre a la comunidad.  </p>
+	<h1> Pósters </h1>
+	<p><b>CRoNe 2015</b> tiene contemplado aprovechar la convocatoria de los expositores de nivel internacional para dar una realimentación a tu investigación o desarrollo. Para ello se ha agendado un tiempo para la <b>exposición de pósters</b> en la cual además de los expositores y los asistentes al evento, habrá un gran flujo de personas en la universidad por ser la semana en que ésta se abre a la comunidad.  </p>
 
-	<h2> Reglas para los posters </h2>
-
-	<p>El poster final debe seguir las siguientes indicaciones.</p>
-	<ul>
-		<li>Todas las figuras y textos deben ser preparados previamente. El afiche NO consiste en simplemente pegar el resumen extendido en un fichero. </li>
-		<li>El título completo de la presentación y los nombres de todos los autores deben aparecer en la parte superior del afiche. El tamaño del texto no debe ser de menos de 48 pts. para el título y un poco menos para los nombres de los autores. El subtítulo no debe ser menor a 20 pts.</li>
-		<li>
-			El tamaño sugerido para el póster es A0, formato retrato (portrait): 840 mm/33″ largo por 1188/47″ de alto; el alto máximo del afiche es 950 mm/37″ largo por 1200/47″ de alto.
-		</li>
-		<li>Un CD con el póster, hecho de preferencia en Freehand o equivalente (Adobe Illustrator), debe ser entregado.</li>
-		<li>Cualquier tipo de plagio significará la eliminación del concurso.</li>
-	</ul>
 	
-	
-	
-	
-
-	<h2> Normas </h2>
+	<h2> Resumen extendido </h2>
 
 	<p>Se recibirá resumen extendido con las siguientes características</p>
 	<ul>
@@ -47,18 +31,36 @@
 		</li>
 	</ul>
 
-	<h2> Se reciben hasta el 07 de agosto del 2015 </h2>
+	<h2>Procedimiento</h2>
+	<ul>
+		<li>Envía un resumen extendido de tu trabajo de investigación o desarrollo, a poster@crone2015.org hasta el 07 de Agosto</li>
+		<li> El 28 de Agosto recibirás una notificación con la decisión de aceptación de tu trabajo </li>
+		<li>Trae tu póster impreso al evento, según las guías que se te indicarán en el correo de aceptación previamente enviado</li>
+	</ul>
 
-	<p> Se reciben los resumenes extendidos hasta el día 07 de agosto del 2015 por mail en poster@crone2015.org </p>
 
-
-
-	<h2> Notificación de resultados: 28 de agosto. </h2>
-
-	<p>Los posters serán seleccionados basados en la calidad del resumen extendido. La notificación de la decisión será enviada por correo electrónico.</p>
-
-	<h2> Exposición de posters: viernes 24 de Octubre del 2015 </h2>
-	<p> Se expondrán lo posters a toda la comunidad de valparaíso y a los expositores quienes daran su realimentación. </p>
+	<h2>Temas de interés</h2>
+	<p>Los temas de interés incluyen, pero no se limitan a:</p>
+	<ul>
+	<li>Aprendizaje de máquinas</li>
+	<li>Robótica Cognitiva</li>
+	<li>Aplicaciones de control automático</li>
+	<li> Aplicaciones de machine learning</li>
+	<li>Aplicaciones de visión computacional</li>
+	<li>Inteligencia artificial en robótica</li>
+	<li>Redes neuronales en robótica</li>
+	<li>Robótica cognitiva</li>
+	<li>Robótica de enjambre</li>
+	<li>Sistemas teleoperados</li>
+	</ul>
+	
+	
+	<h2>Fechas Importantes</h2>
+	<ul>
+		<li>Envío de resumen extendido: 07 de agosto</li>
+		<li>Notificación de resultados: 28 de agosto</li>
+		<li>Exposición: 23 de octubre</li>
+	</ul>
 
 
 </div>
