2361ff1be4c05c75018e68362bcde5781954ba13
psigelo <pascal.sigel@gmail.com>
2015-05-25 00:46:56 -0300

    Se ha agregado la seccion poster

--- web/index.php
+++ web/index.php
@@ -6,7 +6,7 @@
 	<!-- <div class="container"> -->
 		<div id="owl-demo" class="owl-carousel owl-theme">
 			<div class="item">
-				<img src="images/banner2.jpg" alt="Crabot">
+				<img src="images/banner.jpg" alt="Crabot">
 				<div class="banner-title"> 
 					<h4>1<sup>er</sup> Congreso de Robótica y Neurociencia</h4>
 					<p>CRoNe es un punto de encuentro para estudiantes de pregrado y postgrado, junto con algunos de los expertos mundiales en Robótica y Neurociencia. Este evento tomará lugar del <b>21 al 23 de Octubre</b>, en la UTFSM. Revisa esta web para mantenerte actualizado acerca de nuestros expositores invitados.</p>
@@ -35,21 +35,7 @@
 
 
 <!--//expo-->
-<!--posters-->
-<div class="poster" id="poster">
-	<div class="container">
-		<h3>Llamado a Posters</h3>
-		<div class="poster-grids"></div>
-	</div>
-	<div class="poster-img">
-		<img src="images/banner-posters-square.png" alt=""/>
-		<div class="poster-title"> 
-			<h4>¡Ven y presenta tu trabajo!</h4>
-			<p>Muestra tu trabajo de investigación o desarrollo en robótica y/o neurociencia, un comité de expositores te darán excelente retroalimentación. </br></br> Envía tu resumen extendido a <b>poster@crone2015.org</b> </br></br> Plazo hasta el <b>07 de Agosto</b></p>
-		</div>
-	</div>
-</div>
-<!--//posters-->
+
 <!--activity-->
 <div class="activity" id="activity">
 	<div class="container">
@@ -275,6 +261,27 @@
 	</div>
 </div>	
 <!--//activity-->
+
+
+<!--posters-->
+<div class="poster" id="poster" >
+	<div class="container">
+		<h3>Llamado a Posters</h3>
+		<div class="poster-grids"></div>
+	</div>
+	<div class="poster-img">
+	 	<a href="poster.php">
+			<img src="images/banner-posters-square.png" alt=""/>
+		
+			<div class="poster-title"> 
+				<h4>¡Ven y presenta tu trabajo!</h4>
+				<p>Muestra tu trabajo de investigación o desarrollo en robótica y/o neurociencia, un comité de expositores te darán excelente retroalimentación. </br></br> Envía tu resumen extendido a <b>poster@crone2015.org</b> </br></br> Plazo hasta el <b>07 de Agosto</b></p>
+			</div>
+		</a>
+	</div>
+</div>
+<!--//posters-->
+
 <?php
 	echo "
 	<script>
--- web/poster.php
+++ web/poster.php
@@ -1,11 +1,72 @@
 <?php $current_page="poster"; include_once('header.php'); ?>
 <!--posters-->
-
-	<img class="poster-img" src="images/banner-posters-square.png" alt=""/>
-
+<div class="poster-img" >
+	<img src="images/banner-posters-square.png" alt=""/>
+	<div class="poster-title"> 
+		<h4>¡Ven y presenta tu trabajo!</h4>
+		<p>Muestra tu trabajo de investigación o desarrollo en robótica y/o neurociencia, un comité de expositores te darán excelente retroalimentación. </br></br> Envía tu resumen extendido a <b>poster@crone2015.org</b> </br></br> Plazo hasta el <b>07 de Agosto</b></p>
+	</div>
+</div>
 <!--//posters-->
 <br \>
+<div class="posterDetailConteiner">
+	<h1> Posters </h1>
+	<p><b>CRoNe 2015</b> tiene contemplado aprovechar la convocatoria de los expositores de nivel internacional para dar una realimentación a tu ingestivación, para ello se agendado un tiempo para la <b>exposición de los poster</b> en la cual además de los expositores y los asistentes al evento habrá un gran flujo de personas en la universidad por ser la semana en que la universidad se abre a la comunidad.  </p>
 
-<h1> Detalles de la postulación </h1>
-<p>asd</p>
+	<h2> Reglas para los posters </h2>
+
+	<p>El poster final debe seguir las siguientes indicaciones.</p>
+	<ul>
+		<li>Todas las figuras y textos deben ser preparados previamente. El afiche NO consiste en simplemente pegar el resumen extendido en un fichero. </li>
+		<li>El título completo de la presentación y los nombres de todos los autores deben aparecer en la parte superior del afiche. El tamaño del texto no debe ser de menos de 48 pts. para el título y un poco menos para los nombres de los autores. El subtítulo no debe ser menor a 20 pts.</li>
+		<li>
+			El tamaño sugerido para el póster es A0, formato retrato (portrait): 840 mm/33″ largo por 1188/47″ de alto; el alto máximo del afiche es 950 mm/37″ largo por 1200/47″ de alto.
+		</li>
+		<li>Un CD con el póster, hecho de preferencia en Freehand o equivalente (Adobe Illustrator), debe ser entregado.</li>
+		<li>Cualquier tipo de plagio significará la eliminación del concurso.</li>
+	</ul>
+	
+	
+	
+	
+
+	<h2> Normas </h2>
+
+	<p>Se recibirá resumen extendido con las siguientes características</p>
+	<ul>
+		  <li>Escrito en inglés.</li>
+		  <li>Máximo 3 páginas.</li>
+		  <li>Las siguientes secciones deben ir:
+		  	<ul>
+			  <li>Introducción</li>
+			  <li>Métodos</li>
+			  <li>Resultados</li>
+			  <li>Discusión</li>
+			  <li>Conclusión</li>
+			</ul>
+		</li>
+	</ul>
+
+	<h2> Se reciben hasta el 07 de agosto del 2015 </h2>
+
+	<p> Se reciben los resumenes extendidos hasta el día 07 de agosto del 2015 por mail en poster@crone2015.org </p>
+
+
+
+	<h2> Notificación de resultados: 28 de agosto. </h2>
+
+	<p>Los posters serán seleccionados basados en la calidad del resumen extendido. La notificación de la decisión será enviada por correo electrónico.</p>
+
+	<h2> Exposición de posters: viernes 24 de Octubre del 2015 </h2>
+	<p> Se expondrán lo posters a toda la comunidad de valparaíso y a los expositores quienes daran su realimentación. </p>
+
+
+</div>
+<br \>
+<br \>
+<br \>
+<br \>
+<br \>
+<br \>
+
 <?php include_once('footer.php'); ?>
